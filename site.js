3ac9aeaeb2da1a139a0054b156622cb117d616e1
Yusuf Kelo <yusufkel@gmail.com>
2015-10-06 21:32:34 +0100

    update

--- site.js
+++ site.js
@@ -1,9 +1,52 @@
 (function(){
 	
-	var app=angular.module('app',['ui.router'])
+	angular.module('app',['ui.router'])
+	.run(function($rootScope,$http){
+		
+		    var statuses={
+				inPreparation:'In Preparation'
+				,readyToShip:'Ready to ship'
+				,shipped:'Shipped'
+			}
+		
+			$rootScope.orders={};
+				$rootScope.orders.list=[
+			{orderId:1,itemName:'Milk',quantity:10,totalPrice:444,unitPrice:10,orderDate:'Monday 1, july 2015',status:statuses.inPreparation},
+			{orderId:2,itemName:'Play station',quantity:5,totalPrice:55,unitPrice:3,orderDate:'Monday 1, july 2015',status:statuses.inPreparation},
+			{orderId:3,itemName:'Sony CD',quantity:6,totalPrice:88,unitPrice:35,orderDate:'Monday 1, july 2015',status:statuses.readyToShip},
+			{orderId:4,itemName:'Iphone',quantity:7,totalPrice:99,unitPrice:87,orderDate:'Monday 1, july 2015',status:statuses.inPreparation},
+		];
+		
+		$rootScope.prepareStats=function(){
+			
+			var inPreparation=0;
+			var readyToShip=0;
+			var shipped=0;
+			
+			for(var i=0;i<$rootScope.orders.list.length;++i){
+				var item=$rootScope.orders.list[i];
+		
+				if(item.status==statuses.inPreparation){
+					
+					++inPreparation;
+				}
+				else if(item.status==statuses.readyToShip){
+					++readyToShip;
+				}
+				else if(item.status==statuses.shipped){
+					++shipped;
+				}
+			}          
+			$rootScope.inPreparation=inPreparation;
+			$rootScope.readyToShip=readyToShip;
+			$rootScope.shipped=shipped;
+			
+		}
+	})
 	.config(function($stateProvider, $urlRouterProvider){
 		
 		$stateProvider
+		
 		.state('summary',{
 			url:'/summary',
 			templateUrl:'summary.html'
@@ -19,7 +62,6 @@
 			templateUrl:'edit.html'
 			,controller:'editController'
 		})
-		
 		$urlRouterProvider.otherwise('/summary')
 	})
 	.controller('summaryController',function($scope,$rootScope){
@@ -27,41 +69,34 @@
 			$rootScope.title='Orders Summary';
 		
 		$scope.orders={};
-		$scope.orders.summary=[
-			{orderId:1,numberOfItems:10,totalOrderPrice:444,orderDate:'Monday 1, july 2015',status:'Delivered'},
-			{orderId:2,numberOfItems:33,totalOrderPrice:77,orderDate:'Tuesday 2, july 2015',status:'Pending'},
-			{orderId:3,numberOfItems:55,totalOrderPrice:92,orderDate:'Wednesday 4, july 2015',status:'Pending'},
-			{orderId:4,numberOfItems:100,totalOrderPrice:60,orderDate:'Thursday  5, july 2015',status:'Delivered'},
-		]
+		$scope.orders.summary=$scope.orders.list=$rootScope.orders.list;
 		
-	
 		
 	})
 	.controller('listController',function($scope,$rootScope,$state){
 		$rootScope.title='Orders List';
+		$rootScope.prepareStats();
+		
+		$scope.orders.list=$rootScope.orders.list;
 		
 		
-		$scope.orders={};
-				$scope.orders.list=[
-			{orderId:1,itemName:10,quantity:10,totalPrice:444,unitPrice:10,orderDate:'Monday 1, july 2015',status:'Delivered'},
-			{orderId:2,itemName:12,quantity:5,totalPrice:55,unitPrice:3,orderDate:'Monday 1, july 2015',status:'Delivered'},
-			{orderId:3,itemName:14,quantity:6,totalPrice:88,unitPrice:35,orderDate:'Monday 1, july 2015',status:'Pending'},
-			{orderId:4,itemName:16,quantity:7,totalPrice:99,unitPrice:87,orderDate:'Monday 1, july 2015',status:'Delivered'},
-		]
+		
+	})
+	.controller('editController',function($scope,$rootScope,$stateParams,$state){
+		$rootScope.title='Edit Order';
+	
+		$scope.order=$rootScope.orders.list[$stateParams.id];
 		
 		$scope.controller={
-			edit:function(item){
-				$state.go('edit',{id:1})
+			save:function(){
+				$rootScope.prepareStats()
+				$state.go('orders')
+				
 			}
 		}
-		
 	})
-	.controller('editController',function($scope,$rootScope){
-		$rootScope.title='Edit Order';
+	
 		
-	})
-		
-		console.log('hello')
 	
 })()
 
